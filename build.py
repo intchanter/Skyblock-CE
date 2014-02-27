d34e7360e90c037ccde0a8eab37d73669a7bdc0b
Daniel Fackrell <dfackrell@bluehost.com>
2014-02-27 23:51:29 +0000

    Now we can clear a bunch of chunks

--- build.py
+++ build.py
@@ -5,7 +5,15 @@ import pymclevel
 
 def main():
     level = pymclevel.MCInfdevOldLevel(settings.output_filename, create=True)
-    print level
+    clear_chunks(level, radius = 0)
+    level.saveInPlace()
+
+def clear_chunks(level, radius = 0):
+    for chunkX in range(-radius, radius + 1):
+        for chunkZ in range(-radius, radius + 1):
+            chunk = level.getChunk(chunkX, chunkZ)
+            chunk.Blocks[chunk.Blocks != -1] = level.materials.Air.ID
+            chunk.chunkChanged()
 
 if __name__ == '__main__':
     main()
