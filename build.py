01ca20d36e9aadaf967865831b3266fdcb645f7e
Daniel Fackrell <dfackrell@bluehost.com>
2014-02-28 02:22:49 +0000

    Made the dirt island

--- build.py
+++ build.py
@@ -6,14 +6,23 @@ import pymclevel
 def main():
     level = pymclevel.MCInfdevOldLevel(settings.output_filename, create=True)
     clear_chunks(level, radius = 0)
+    dirt_island(level)
     level.saveInPlace()
 
 def clear_chunks(level, radius = 0):
     for chunkX in range(-radius, radius + 1):
         for chunkZ in range(-radius, radius + 1):
             chunk = level.getChunk(chunkX, chunkZ)
-            chunk.Blocks[chunk.Blocks != -1] = level.materials.Air.ID
+            chunk.Blocks[:, :, :] = level.materials.Air.ID
+            #chunk.Blocks[chunk.Blocks != -1] = level.materials.Air.ID
             chunk.chunkChanged()
+
+def dirt_island(level):
+    chunk = level.getChunk(0, 0)
+    chunk.Blocks[0:4, 0:4, 64:68] = level.materials.Dirt.ID
+    chunk.Blocks[4:8, 0:4, 64:68] = level.materials.Dirt.ID
+    chunk.Blocks[0:4, 4:8, 64:68] = level.materials.Dirt.ID
+    chunk.chunkChanged()
 
 if __name__ == '__main__':
     main()
