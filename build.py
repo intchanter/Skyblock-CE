5a5644848ff615e2736d4b48d4eaf7be60de287b
Daniel Fackrell <dfackrell@bluehost.com>
2014-02-28 06:28:31 +0000

    Added sand island and first two chests

--- build.py
+++ build.py
@@ -1,12 +1,23 @@
 #!/bin/env python
 
 import settings
-import pymclevel
+from pymclevel import MCInfdevOldLevel
+from pymclevel import TileEntity
+from pymclevel import TAG_Compound
+from pymclevel import TAG_List
+from pymclevel import TAG_Short
+from pymclevel import TAG_Byte
+from pymclevel import TAG_String
+
 
 def main():
-    level = pymclevel.MCInfdevOldLevel(settings.output_filename, create=True)
-    clear_chunks(level, radius = 0)
+    level = MCInfdevOldLevel(settings.output_filename, create=True)
+    level.setPlayerPosition( (2, 67, 2) )
+    level.setPlayerSpawnPosition( (2, 64, 2) )
+    clear_chunks(level, radius = 12)
     dirt_island(level)
+    sand_island(level)
+    level.generateLights()
     level.saveInPlace()
 
 def clear_chunks(level, radius = 0):
@@ -14,14 +25,46 @@ def clear_chunks(level, radius = 0):
         for chunkZ in range(-radius, radius + 1):
             chunk = level.getChunk(chunkX, chunkZ)
             chunk.Blocks[:, :, :] = level.materials.Air.ID
-            #chunk.Blocks[chunk.Blocks != -1] = level.materials.Air.ID
             chunk.chunkChanged()
 
 def dirt_island(level):
     chunk = level.getChunk(0, 0)
-    chunk.Blocks[0:4, 0:4, 64:68] = level.materials.Dirt.ID
-    chunk.Blocks[4:8, 0:4, 64:68] = level.materials.Dirt.ID
-    chunk.Blocks[0:4, 4:8, 64:68] = level.materials.Dirt.ID
+
+    # Dirt
+    dirt_id = level.materials.Dirt.ID
+    chunk.Blocks[0:4, 0:4, 60:64] = dirt_id
+    chunk.Blocks[4:8, 0:4, 60:64] = dirt_id
+    chunk.Blocks[0:4, 4:8, 60:64] = dirt_id
+
+    # Grass
+    grass_id = level.materials.Grass.ID
+    dirt = chunk.Blocks[:, :, 63] == dirt_id
+    chunk.Blocks[:, :, 63][dirt] = grass_id
+    chunk.chunkChanged()
+
+    # Chest
+    chest_id = level.materials.Chest.ID
+    chunk.Blocks[7, 1, 64] = chest_id
+    chunk.Data[7, 1, 64] = 4
+    chest_inventory = level.tileEntityAt(7, 1, 64)
+
+    print(chest_inventory)
+    print(dir(chest_inventory))
+
+
+def sand_island(level):
+    chunk = level.getChunk(-4, 0)
+
+    # Sand
+    sand_id = level.materials.Sand.ID
+    chunk.Blocks[0:4, 0:4, 60:64] = sand_id
+
+    # Chest
+    chest_id = level.materials.Chest.ID
+    chunk.Blocks[7, 1, 64] = chest_id
+    chunk.Data[7, 1, 64] = 4
+    chest_inventory = level.tileEntityAt(7, 1, 64)
+
     chunk.chunkChanged()
 
 if __name__ == '__main__':
