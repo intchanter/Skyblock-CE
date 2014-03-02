b8fe39e81e79d3f8ccc7c48802c760793dccdf0c
Daniel Fackrell <dfackrell@bluehost.com>
2014-03-02 05:58:34 +0000

    Refactored and added chest code.  Backed out crash-causing code

--- build.py
+++ build.py
@@ -11,22 +11,56 @@ from pymclevel import TAG_String
 
 
 def main():
-    level = MCInfdevOldLevel(settings.output_filename, create=True)
-    level.setPlayerPosition( (2, 67, 2) )
-    level.setPlayerSpawnPosition( (2, 64, 2) )
-    clear_chunks(level, radius = 12)
+    # Set random_seed explicitly just to avoid randomness
+    level = MCInfdevOldLevel(settings.output_filename, create=True, random_seed=1)
+
+    # Place the player
+    px, py, pz = (2, 64, 2)
+    level.setPlayerPosition( (px, py + 2, pz) )
+    level.setPlayerSpawnPosition( (px, py, pz) )
+    #print(dir(level.materials))
+    #print(dir(level))
+
+    create_empty_chunks(level, radius=12)
+
     dirt_island(level)
     sand_island(level)
     soul_sand_island(level)
     bedrock_island(level)
     obsidian_island(level)
     portal_island(level)
+
     level.generateLights()
     level.saveInPlace()
 
-def clear_chunks(level, radius = 0):
+def item_stack(item):
+    item_tag = TAG_Compound()
+    item_tag.name = ''
+    item_tag['id'] = TAG_Short(item['id'])
+    item_tag['Damage'] = TAG_Short(item['damage'])
+    item_tag['Count'] = TAG_Byte(item['count'])
+    item_tag['Slot'] = TAG_Byte(item['slot'])
+    return item_tag
+
+def make_chest(level, chunk, pos, contents):
+    x, z, y = pos
+    chest_id = level.materials.Chest.ID
+    chunk.Blocks[x, z, y] = chest_id
+    chunk.Data[x, z, y] = 4  # facing west
+    chest = TileEntity.Create('Chest')
+    TileEntity.setpos(chest, (x, z, y))
+    for item in contents:
+        chest['Items'].append(item_stack(item))
+    print(chest['Items'])
+    # TODO: The following line crashes the client.  Find out why!
+    # It appears to be triggered by the one in the bedrock island.
+    # Possibility: the TileEntity is falling out of the world?
+    chunk.TileEntities.append(chest)
+
+def create_empty_chunks(level, radius = 0):
     for chunkX in range(-radius, radius + 1):
         for chunkZ in range(-radius, radius + 1):
+            level.createChunk(chunkX, chunkZ)
             chunk = level.getChunk(chunkX, chunkZ)
             chunk.Blocks[:, :, :] = level.materials.Air.ID
             chunk.chunkChanged()
@@ -48,14 +82,13 @@ def dirt_island(level):
     chunk.chunkChanged()
 
     # Chest
-    chest_id = level.materials.Chest.ID
-    chunk.Blocks[7, 1, 64] = chest_id
-    chunk.Data[7, 1, 64] = 4
-    chest_inventory = level.tileEntityAt(7, 1, 64)
-
-    print(chest_inventory)
-    print(dir(chest_inventory))
-
+    contents = [
+            {'id': level.materials.Ice.ID,
+                'count': 1, 'damage': 0, 'slot': 0},
+            #{'id': level.materials.LavaBucket.ID,
+                #'count': 1, 'damage': 0, 'slot': 0},
+            ]
+    make_chest(level, chunk, (7, 1, 64), contents)
 
 def sand_island(level):
     # Main
@@ -70,10 +103,8 @@ def sand_island(level):
     chunk.Blocks[0, 3, 64] = cactus_id
 
     # Chest
-    chest_id = level.materials.Chest.ID
-    chunk.Blocks[7, 1, 64] = chest_id
-    chunk.Data[7, 1, 64] = 4
-    chest_inventory = level.tileEntityAt(7, 1, 64)
+    contents = []
+    make_chest(level, chunk, (2, 2, 64), contents)
 
     chunk.chunkChanged()
 
@@ -93,6 +124,16 @@ def bedrock_island(level):
     air_id = level.materials.Air.ID
     chunk.Blocks[1:7, 1:7, 1:7] = air_id
     chunk.Blocks[:, :, 5] = air_id
+
+    # End portal frame
+    frame_id = level.materials.PortalFrame.ID
+    chunk.Blocks[1:6, 2:5, 1] = frame_id
+    chunk.Blocks[2:5, 1:6, 1] = frame_id
+    chunk.Blocks[2:5, 2:5, 1] = air_id
+
+    # Chest
+    contents = []
+    #make_chest(level, chunk, (3, 3, 1), contents)
 
     chunk.chunkChanged()
 
